d734887dc6cbad032b86a5c2243d3283a3ccd2ff
ryanjinx0002 <ryanjinx0002@users.noreply.github.com>
2026-07-16 16:32:05 +0530

    new change

--- start/variables.js
+++ start/variables.js
@@ -2,3 +2,10 @@
 let a = 'ok' 
 console.log("answer:", a) //print1
 console.log(`answer = ${a}`)//print2
+//boolean
+let boolean = true
+console.log(`boolean = ${boolean}`)
+//naming conventions 
+let firstName = "john";
+let lastName = "Doe";
+console.log(`firstName = ${firstName}, lastName = ${lastName}`)
