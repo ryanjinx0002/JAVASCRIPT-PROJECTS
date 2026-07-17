1987bd75bc6d2afa1799527fdd2686418865ce14
ryanjinx0002 <ryanjinx0002@users.noreply.github.com>
2026-07-17 22:39:36 +0530

    new syllabus covered

---
+++ start/operator.js
@@ -0,0 +1,30 @@
+let a=10
+let b=5 
+let c=a+b
+let d=a-b
+let e=a*b
+let f=a/b
+let g=a%b//modulo operator (remainder)
+
+let number = ("enter any no here to run program ")
+if (number % 2 === 1) {// === means strict equality, it checks both value and type
+    console.log("odd");
+} else {
+    console.log("even");
+}
+//shortcut methods of airthmetic operations
+let count = 0
+count += 1 // count = count + 1
+count -= 1 // count = count - 1
+count /= 1 // count = count / 1
+count *= 1 // count = count * 1
+count %= 1 // count = count % 1
+console.log(count)
+//checking operators
+// Type your code below
+let n1=8
+let n2=9
+let n3= n1>n2
+let boolean=n3
+// Don't change the line below
+console.log(`n1 = ${n1}, n2 = ${n2}, n3 = ${n3}`)
