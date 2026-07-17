ecf00b438e80c540285e1e6a8d22421399352fae
ryanjinx0002 <ryanjinx0002@users.noreply.github.com>
2026-07-17 16:37:18 +0530

    new updates

--- start/variables.js
+++ start/variables.js
@@ -7,5 +7,15 @@ let boolean = true
 console.log(`boolean = ${boolean}`)
 //naming conventions 
 let firstName = "john";
-let lastName = "Doe";
+let lastName = "Doe";//its initialized, so it will be a string
 console.log(`firstName = ${firstName}, lastName = ${lastName}`)
+//empty variable
+let emptyVariable;//its uninitialized, so it will be undefined
+emptyVariable = "a"
+console.log(emptyVariable)//print3
+//constants
+const pi = 3.14
+const score = 50;
+// score = 75; // This would cause an error since constants cannot be reassigned
+console.log(score)
+console.log(pi)
