82e9422e5bc041f5187d7e3f9d13e2e5c106d80d
ryanjinx0002 <ryanjinx0002@users.noreply.github.com>
2026-07-23 22:13:53 +0530

    after 4 day break ig

--- operator pt2.js
+++ operator pt2.js
@@ -1,7 +1,23 @@
-// Type your code below
-let b1 = (5>10)
+/*// Type your code below
+let b1 = (50>10)
 let b2 = (100<10)
 let b3 = b1 || b2
 
 // Don't change the line below
 console.log(`b3 = ${b3}`)
+
+// Type your code below
+let b1 = 4
+let b2 = 3
+let b3 = !((b1 + b2) > (b1 * b2))
+  
+// Don't change the line below
+console.log(`b3 = ${b3}`)*/
+
+let b1 = 3>1
+let b2 = 5>4
+let b3 = 4<3
+
+// Don't change the line below
+let b4 = b1 && b2 && (!b3)
+console.log(`b4 = ${b4}`)
